df836606a4780967d524771822f9842b583c9a7b
ThuChuc0812 <chucnttph32240@fpt.edu.vn>
2024-11-07 22:00:53 +0700

    bookingPage + payment

--- hotel-website/src/App.js
+++ hotel-website/src/App.js
@@ -5,11 +5,12 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Rooms from './pages/Rooms';
-import Bookings from './pages/Bookings';
 import Login from './components/Login';
 import Register from './components/Register';
 import Profile from './components/Profile';
 import Footer from './components/Footer';
+import BookingPage from './pages/BookingPage';
+import PaymentPage from './pages/PaymentPage';
 function App() {
   return (
     <div className="app-container">
@@ -19,11 +20,11 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/rooms" element={<Rooms />} />
-            <Route path="/bookings" element={<Bookings />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
             <Route path="/profile" element={<Profile />} />
-            
+            <Route path="/booking" element={<BookingPage />} />
+            <Route path="/payment" element={<PaymentPage />} />
           </Routes>
         </div>
       </div>
