a07c2076f217aab1795e2378deb2402c620c7e53
Ntson2k4 <sonntph32136@fpt.edu.vn>
2025-05-03 15:20:19 +0700

    fix pt

--- React/ems-fronend/src/components/HoaDon/Demo.jsx
+++ React/ems-fronend/src/components/HoaDon/Demo.jsx
@@ -262,7 +262,7 @@ const Demo = () => {
                 };
 
                 await ThemPhuThu(phuThuRequest);
-                Swal.fire("Thành công", `Đã tạo phụ thu do trả phòng muộn`, "success");
+                Swal.fire("Thành công",`Đã tạo phụ thu do trả phòng muộn cho phòng ${item.xepPhong.tenPhong}`, "success");
             }
 
             localStorage.setItem('traPhong', JSON.stringify(selectedItems));
