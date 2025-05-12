5b0fd617d53760b3f201b5cfd93a594d98e306f5
bui hoang long <builong1710@gmail.com>
2025-05-12 13:05:41 +0700

    fix thongke

--- src/main/java/com/example/datn/repository/ThanhToanRepository.java
+++ src/main/java/com/example/datn/repository/ThanhToanRepository.java
@@ -12,41 +12,38 @@ import java.util.List;
 @Repository
 public interface ThanhToanRepository extends JpaRepository<ThanhToan, Integer> {
 
-    @Query("SELECT FUNCTION('YEAR', tt.ngayThanhToan) AS doanhThuNam, " +
-            "FUNCTION('MONTH',tt.ngayThanhToan) AS doanhThuThang, " +
-            "(SELECT SUM(ttSub.tienThanhToan) FROM ThanhToan ttSub WHERE ttSub.hoaDon.id = hd.id GROUP BY ttSub.hoaDon.id) AS tongDoanhThu, " +
+    @Query("SELECT FUNCTION('YEAR', hd.ngayTao) AS doanhThuNam, " +
+            "FUNCTION('MONTH', hd.ngayTao) AS doanhThuThang, " +
+            "SUM(DISTINCT hd.tongTien) AS tongDoanhThu, " +
             "SUM(tthd.tienPhong) AS doanhThuPhong, " +
             "SUM(tthd.tienDichVu) AS doanhThuDichVu, " +
             "SUM(tthd.tienPhuThu) AS doanhThuPhuThu " +
             "FROM HoaDon hd " +
-            "JOIN ThongTinHoaDon tthd ON hd.id = tthd.hoaDon.id " +
-            "JOIN ThanhToan tt ON hd.id = tt.hoaDon.id " +
-            "GROUP BY FUNCTION('YEAR',tt.ngayThanhToan), FUNCTION('MONTH', tt.ngayThanhToan), hd.id " +
-            "ORDER BY FUNCTION('YEAR', tt.ngayThanhToan), FUNCTION('MONTH',tt.ngayThanhToan)")
+            "LEFT JOIN ThongTinHoaDon tthd ON hd.id = tthd.hoaDon.id " +
+            "GROUP BY FUNCTION('YEAR', hd.ngayTao), FUNCTION('MONTH', hd.ngayTao), hd.id " +
+            "ORDER BY FUNCTION('YEAR', hd.ngayTao), FUNCTION('MONTH', hd.ngayTao)")
     List<Object[]> thongKeDoanhThu();
 
 
-    @Query("SELECT FUNCTION('YEAR',tt.ngayThanhToan) AS doanhThuNam, " +
-            "FUNCTION('MONTH',tt.ngayThanhToan) AS doanhThuThang, " +
+    @Query("SELECT FUNCTION('YEAR', hd.ngayTao) AS doanhThuNam, " +
+            "FUNCTION('MONTH', hd.ngayTao) AS doanhThuThang, " +
             "lp.tenLoaiPhong, COUNT(lp.tenLoaiPhong) " +
             "FROM ThongTinDatPhong ttdp " +
             "JOIN HoaDon hd ON hd.datPhong.id = ttdp.datPhong.id " +
             "JOIN LoaiPhong lp ON lp.id = ttdp.loaiPhong.id " +
-            "JOIN ThanhToan tt ON tt.hoaDon.id = hd.id "+
             "WHERE ttdp.trangThai IN :trangThai " +
-            "GROUP BY FUNCTION('YEAR',tt.ngayThanhToan), FUNCTION('MONTH',tt.ngayThanhToan), lp.tenLoaiPhong")
+            "GROUP BY FUNCTION('YEAR', hd.ngayTao), FUNCTION('MONTH', hd.ngayTao), lp.tenLoaiPhong")
     List<Object[]> thongKeLoaiPhong(List<String> trangThai);
 
-    @Query("SELECT FUNCTION('YEAR', tt.ngayThanhToan) AS doanhThuNam, " +
-            "FUNCTION('MONTH', tt.ngayThanhToan) AS doanhThuThang, " +
+    @Query("SELECT FUNCTION('YEAR',  hd.ngayTao) AS doanhThuNam, " +
+            "FUNCTION('MONTH',  hd.ngayTao) AS doanhThuThang, " +
             "dvsd.dichVu.tenDichVu, SUM(dvsd.soLuongSuDung) " +
             "FROM ThongTinDatPhong ttdp " +
             "JOIN HoaDon hd ON hd.datPhong.id = ttdp.datPhong.id " +
             "JOIN XepPhong xp ON ttdp.id = xp.thongTinDatPhong.id " +
             "JOIN DichVuSuDung dvsd ON xp.id = dvsd.xepPhong.id " +
-            "JOIN ThanhToan tt ON tt.hoaDon.id = hd.id " +
-            "WHERE dvsd.trangThai = true " +
+            "WHERE dvsd.trangThai = true AND dvsd.giaSuDung != 0 " +
             "AND ttdp.trangThai IN :trangThai " +
-            "GROUP BY FUNCTION('YEAR', tt.ngayThanhToan), FUNCTION('MONTH', tt.ngayThanhToan), dvsd.dichVu.tenDichVu")
+            "GROUP BY FUNCTION('YEAR',  hd.ngayTao), FUNCTION('MONTH',  hd.ngayTao), dvsd.dichVu.tenDichVu")
     List<Object[]> thongKeDichVu(List<String> trangThai);
 }
